ba54331899c2e6c9c35213d8e099cf76d7aa019e
grittycoder <53576986+grittycoder@users.noreply.github.com>
2026-03-15 09:32:18 -0400

    Update App.js

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,1 +1,49 @@
-#
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Import the screens we created
+import LoginScreen from './src/screens/LoginScreen';
+import ServicesScreen from './src/screens/ServicesScreen';
+import BookingScreen from './src/screens/BookingScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#fff' },
+          headerTintColor: '#007AFF',
+          headerTitleStyle: { fontWeight: 'bold' },
+        }}
+      >
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} // Hide header on login
+        />
+        <Stack.Screen 
+          name="Services" 
+          component={ServicesScreen} 
+          options={{ title: 'Available Services' }}
+        />
+        <Stack.Screen 
+          name="Booking" 
+          component={BookingScreen} 
+          options={{ title: 'Book Appointment' }}
+        />
+        <Stack.Screen 
+          name="Payment" 
+          component={PaymentScreen} 
+          options={{ title: 'Checkout' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
